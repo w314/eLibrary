06ca706185d273a0746b16d6000a68a025c419a6
w314 <piroska.werner@gmail.com>
2024-10-13 16:05:37 -0500

    feat Passing login data with GET request via url

--- httpServer.js
+++ httpServer.js
@@ -2,11 +2,29 @@
 // load required modules
 const dbModule = require('./DBModule');
 const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
 
 const server = http.createServer((req, res) => {
 
+    // retrieve data from url
+    // get url
+    const reqUrl = req.url;
+    console.log(`Request URL: ${reqUrl}`);
+    // get path from url
+    const reqPath = url.parse(reqUrl).path;
+    console.log(`Request path: ${reqPath}`);
+    // get querystring from url
+    const reqQuery = url.parse(reqUrl).query;
+    console.log(`Request querystring: ${reqQuery}`);
+    // get data from url
+    const username = querystring.parse(reqQuery)['username'];
+    const password = querystring.parse(reqQuery)['password'];
+    console.log(`Username: ${username}\nPassword: ${password}`);
+
+
     // authenticate user
-    const userStatus = dbModule.authenticateUser('admin', 'admin');
+    const userStatus = dbModule.authenticateUser(username, password);
 
     // write response
     res.writeHead(200, { ContentType: "text/html" });
