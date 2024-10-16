31a14cc3ac22c59b6a31be47f50d82d036c54115
w314 <piroska.werner@gmail.com>
2024-10-16 09:15:14 -0500

    feat: Log user registration in log.txt file

--- httpServer.js
+++ httpServer.js
@@ -1,51 +1,11 @@
 
-// load required modules
-// const dbModule = require('./DBModule');
 import http from 'http';
-// const querystring = require('querystring');
-// load custom event handler module
-// const eventModule = require('./EventHandlerModule');
-// const routerModule = require('./routes/router')
+// import router
 import router from './router/router.js'
 
-// const url = require('url');
-// import * as url from 'url';
-
 const server = http.createServer((req, res) => {
-
-    // // RETRIEVE DATA FROM REQUEST BODY
-
-    // // create variable to store incoming data chunks
-    // let data = '';
-    // req.on('data', (chunk) => data += chunk);
-    // req.on('end', () => {
-
-    //     // parse received request data with querystring
-    //     const reqData = querystring.parse(data);
-    //     // get username and password from parsed request data
-    //     const username = reqData['username'];
-    //     const password = reqData['password'];
-    //     console.log(`Username: ${username}\nPassword: ${password}`);
-         
-    //     // authenticate user
-    //     const userStatus = dbModule.authenticateUser(username, password);
-    //     // get visitor count
-    //     const visitorCount = eventModule.visitorCountEvent();
-        
-    //     // write response
-    //     res.writeHead(200, { ContentType: "text/html" });
-    //     // stops browsers creating requests for favicon 
-    //     // and therefore adding additional visitor counts
-    //     res.write(`<html><head><link rel="icon" href="data:,"></head>`);
-    //     res.write(`<body><h1>${userStatus}</h1>`);
-    //     res.write(`<p>The number of visitors whe visited the site: ${visitorCount}</p>`);
-    //     res.write(`</body></html>`);
-    //     res.end();
-    // })
-
+    // pass requst to router
     router(req, res);
-
-
 });
 
 // start server
--- router/handlers/loginHandler.js
+++ router/handlers/loginHandler.js
@@ -1,9 +1,14 @@
-import { readFile } from 'node:fs/promises';
+import { readFile, appendFile } from 'node:fs/promises';
 import  qs from 'node:querystring';
 import { authenticateUser } from '../../DBModule.js';
 
-const loginHandler = (req, res) => {
 
+
+const loginHandler = (req, res) => {
+    
+    const logFile = './log.txt';
+    const bookDetailsHtmlPath = './html/bookDetails.html'
+    
     // retrieve data from request
     let data = '';
     req.on('data', (chunk) => data += chunk);
@@ -13,25 +18,27 @@ const loginHandler = (req, res) => {
 
         // get data from request body
         const reqData = qs.parse(data);
-        console.log(reqData);
         const username = reqData['username'];
         const password = reqData['password'];
         console.log(`Username: ${username}\nPassword: ${password}`);
 
         // authenticate user
-        const userStatus = authenticateUser(username, password)
-    
+        const userStatus = authenticateUser(username, password) 
         // variable to hold page content
         let content;
     
         // set page content
         if(userStatus === 'Valid User') {
-            // get book details html
-            const bookDetailsHtmlPath = './html/bookDetails.html'
+            // log user login
+            appendFile(logFile, `User ${username} logged in at ${new Date()}\n`);
+            // get book details html         
             content = await readFile(bookDetailsHtmlPath, { encode: 'utf-8' });
-            // content = 'Valid User';
         } else {
-            content = 'Invalid User'
+            content = `
+                <html><body><h2>Invalid User Credentials</h2>
+                <button><a href="./home">Try Logging In Again</a></button>
+                </body></html>
+            `
         }
     
         // send response
